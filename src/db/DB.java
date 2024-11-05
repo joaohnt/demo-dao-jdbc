7fd1ca8037d3dc2f405bc35f152d2c2672b9c9a0
joaohnt <jpedrooo.cg@gmail.com>
2024-11-05 02:54:17 -0300

    Creating project and git reposity

---
+++ src/db/DB.java
@@ -0,0 +1,67 @@
+package db;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties prop = loadProperties();
+                String url = prop.getProperty("dburl");
+                conn = DriverManager.getConnection(url, prop);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    private static Properties loadProperties() {
+        try (InputStream fs = DB.class.getClassLoader().getResourceAsStream("db.properties")) {
+            if (fs == null) {
+                throw new DbException("Arquivo db.properties não encontrado no classpath");
+            }
+            Properties prop = new Properties();
+            prop.load(fs);
+            return prop;
+        } catch (IOException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    public static void closeConnection() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeStatement(Statement st) {
+        if (st != null) {
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs) {
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+}
