948c940b7c538a1014f03e3960086b586b20f881
joaohnt <jpedrooo.cg@gmail.com>
2024-11-06 04:27:26 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,5 +34,11 @@ public class Program {
         Seller newSeller = new Seller(null, "Karina", "karina@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted. New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(2);
+        seller.setName("Ningning");
+        sellerDao.update(seller);
+        System.out.println("Uppdate complete");
     }
 }
